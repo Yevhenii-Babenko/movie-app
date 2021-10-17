225a2a1efe027d8391180551730efe991a097aa5
yababenko1 <ybabenko@jappware.com>
2021-10-17 22:12:55 +0300

    something crazy with that vue - fixed with issue of revenue is undefined

--- pages/movies/_movieid.vue
+++ pages/movies/_movieid.vue
@@ -2,58 +2,24 @@
   <div class="container single-movie">
     <NuxtLink class="button" :to="{ name: 'index' }">Back</NuxtLink>
     <Loading v-if="isLoading" />
-    <div v-else class="movie-info">
-      <div class="movie-img">
-        <img
-          :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
-          alt="poster"
-        />
-      </div>
-      <div class="movie-content">
-        <h1>Title: {{ movie.title }}</h1>
-        <p class="movie-fact tagline">
-          <span>Tagline:</span> {{ movie.tagline }}
-        </p>
-        <p class="movie-fact">
-          <span>Released:</span>
-          {{
-            new Date(movie.release_date).toLocaleString('en-us', {
-              month: 'long',
-              day: 'numeric',
-              year: 'numeric',
-            })
-          }}
-        </p>
-        <p class="movie-fact"><span>Duration:</span> {{ movie.runtime }}</p>
-        <p class="movie-fact">
-          <span>Revenue:</span>
-          {{ revenue }}
-        </p>
-        <p class="movie-fact"><span>Overview:</span> {{ movie.overview }}</p>
-      </div>
-    </div>
+    <SingleMovie v-else />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { mapGetters } from 'vuex'
-import api from '~/services/api'
+import SingleMovie from '~/components/SingleMovie.vue'
 
 export default Vue.extend({
   name: 'single-movie',
+  components: {
+    SingleMovie,
+  },
   async mounted() {
-    this.$store.dispatch('fetchSingleMovieById', this.$route.params.movieid)
+    await this.$store.dispatch('fetchSingleMovieById', this.$route.params.movieid)
   },
-  computed: {
-    ...mapGetters(['movie', 'isLoading']),
-    revenue() {
-      return this.movie.revenue.toLocaleString('en-us', {
-        style: 'currency',
-        currency: 'USD',
-      })
-    },
-  },
+  computed: mapGetters(['movie', 'isLoading']),
 })
 </script>
 
@@ -68,48 +34,6 @@ export default Vue.extend({
   .button {
     align-self: flex-start;
     margin-bottom: 32px;
-  }
-  .movie-info {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 32px;
-    color: #fff;
-    @media (min-width: 800px) {
-      flex-direction: row;
-      align-items: flex-start;
-    }
-    .movie-img {
-      img {
-        max-height: 500px;
-        width: 100%;
-        @media (min-width: 800px) {
-          max-height: 700px;
-          width: initial;
-        }
-      }
-    }
-    .movie-content {
-      h1 {
-        font-size: 56px;
-        font-weight: 400;
-      }
-      .movie-fact {
-        margin-top: 12px;
-        font-size: 20px;
-        line-height: 1.5;
-        span {
-          font-weight: 600;
-          text-decoration: underline;
-        }
-      }
-      .tagline {
-        font-style: italic;
-        span {
-          font-style: normal;
-        }
-      }
-    }
   }
 }
 </style>

1395f6ff49cbb604a7e8267faa8e464c4fa384f4
yababenko1 <ybabenko@jappware.com>
2021-10-22 17:16:25 +0300

    added other files to the index

---
+++ components/SingleMovieClassComponent.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="movie-info">
+    <div class="movie-img">
+      <img
+        :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+        alt="poster"
+      />
+    </div>
+    <div class="movie-content">
+      <h1>Title: {{ movie.title }}</h1>
+      <p class="movie-fact tagline">
+        <span>Tagline:</span> {{ movie.tagline }}
+      </p>
+      <p class="movie-fact">
+        <span>Released:</span>
+        {{ released }}
+      </p>
+      <p class="movie-fact"><span>Duration:</span> {{ movie.runtime }}</p>
+      <p class="movie-fact">
+        <span>Revenue:</span>
+        {{ revenue }}
+      </p>
+      <p class="movie-fact"><span>Overview:</span> {{ movie.overview }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import { mapGetters } from 'vuex'
+import { Movie } from '~/types/moviesTypes.interfaces'
+
+@Component({
+  computed: mapGetters(['movie']),
+})
+export default class SingleMovieClassComponent extends Vue {
+  movie!: Movie
+
+  get revenue() {
+    return this.movie?.revenue
+      ? this.movie.revenue.toLocaleString('en-us', {
+          style: 'currency',
+          currency: 'USD',
+        })
+      : 0
+  }
+  get released() {
+    return new Date(this.movie.release_date).toLocaleString('en-us', {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~/assets/scss/layout/single-movie';
+</style>
---
+++ pages/movies/_MovieidClassComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container single-movie">
+    <NuxtLink class="button" :to="{ name: 'index' }">Back</NuxtLink>
+    <Loading v-if="isLoading" />
+    <SingleMovieClassComponent v-else />
+  </div>
+</template>
+
+<script lang="ts">
+import { mapGetters } from 'vuex'
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({
+  computed: mapGetters(['isLoading']),
+})
+export default class MovieidClassComponent extends Vue {
+  async mounted() {
+    await this.$store.dispatch(
+      'fetchSingleMovieById',
+      this.$route.params.movieid
+    )
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/pages/movieid';
+</style>

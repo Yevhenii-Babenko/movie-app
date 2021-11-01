7fedb23db6c350a9ddf39c2f6f3df4b09ae727db
yababenko1 <ybabenko@jappware.com>
2021-11-01 13:21:10 +0200

    added stroyblok components pages and tried to added some content

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,12 @@
 <template>
-  <h1>Hello from About</h1>
+  <section class="container">
+    <component v-if="story.content.component" :key="story.content._uid" :blok="story.content" :is="story.content.component"></component>
+  </section>
 </template>
 
 <script>
 export default {
-  mounted() {
+  /* mounted() {
     this.$storybridge(() => {
       const storyblokInstance = new StoryblokBridge()
 
@@ -16,12 +18,12 @@ export default {
       storyblokInstance.on(['published', 'change'], (event) => {
         this.$nuxt.$router.go({
           path: this.$nuxt.$router.currentRoute,
-          force: true
+          force: true,
         })
       })
     })
-  },
-  async asyncData(context) {
+  }, */
+  /* async asyncData(context) {
     // const fullSlug = (context.route.path == '/' || context.route.path == '') ? 'home' : context.route.path
     const story = await context.app.$storyapi
       .get(`cdn/stories/home`, {
@@ -46,9 +48,68 @@ export default {
         }
       })
     return story
+  }, */
+  data() {
+    return {
+      story: { content: {} },
+    }
+  },
+  mounted() {
+    this.$storybridge(() => {
+      const storyblokInstance = new StoryblokBridge()
+
+      storyblokInstance.on('input', (event) => {
+        if (Object.is(event.story.id, this.story.id)) {
+          this.story.content = event.story.content
+        }
+      })
+      storyblokInstance.on(['published', 'change'], (event) => {
+        this.$nuxt.$router.go({
+          path: this.$nuxt.$router.currentRoute,
+          force: true,
+        })
+      })
+    })
+    // Load the JSON from the API
+    /* this.$storybridge.on(['input', 'published', 'change'], (event) => {
+      if (event.action == 'input') {
+        if (event.story.id === this.story.id) {
+          this.story.content = event.story.content
+        }
+      } else {
+        window.location.reload()
+      }
+    }) */
+  },
+  asyncData(context) {
+    return context.app.$storyapi
+      .get('cdn/stories/home', {
+        version: 'draft',
+      })
+      .then((res) => {
+        return res.data
+      })
+      .catch((res) => {
+        if (!res.response) {
+          console.error(res)
+          context.error({
+            statusCode: 404,
+            message: 'Failed to receive content form api',
+          })
+        } else {
+          console.error(res.response.data)
+          context.error({
+            statusCode: res.response.status,
+            message: res.response.data,
+          })
+        }
+      })
   },
 }
 </script>
 
 <style>
+  .d-flex {
+    display: flex;
+  }
 </style>
---
+++ plugins/components.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Page from '~/storyblok/Page.vue';
+import Teaser from '~/storyblok/Teaser.vue';
+import Grid from '~/storyblok/Grid.vue';
+import Feature from '~/storyblok/Feature.vue';
+import Slide from '~/storyblok/Slide.vue';
+
+Vue.component('page', Page)
+Vue.component('teaser', Teaser)
+Vue.component('grid', Grid)
+Vue.component('feature', Feature)
+Vue.component('slide', Slide)

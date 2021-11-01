951b6836a8ca77b013879d9b17db0c48c5439284
yababenko1 <ybabenko@jappware.com>
2021-11-01 19:30:43 +0200

    added return data of post and some displaing

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,14 +1,33 @@
 <template>
-  <h1>Hello from post root page</h1>
+  <div>
+    <div v-for="post in posts" :key="post.id">
+      <h1>{{post.title}}</h1>
+      <p>{{post.content}}</p>
+      <img :src="post.img" alt="">
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-asyncData(context) {
-    return context.app.$storyapi.get('cdn/stories', {
+  asyncData(context) {
+    return context.app.$storyapi
+      .get('cdn/stories', {
         version: 'draft',
-        starts_with: 'blog/'
-    }).then(response => response)
-}
+        starts_with: 'blog/',
+      })
+      .then((response) => {
+        return {
+          posts: response.data.stories.map((post) => {
+            return {
+              id: post.id,
+              content: post.content.postBody,
+              title: post.content.title,
+              img: post.content.imgArea,
+            }
+          }),
+        }
+      })
+  },
 }
 </script>
